e9faf3b2ff102267cb3de42b79d5e4766b5eca0e
Autioch <autioch@gmail.com>
2018-11-01 22:35:26 +0100

    React setup.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import detectorFactory from 'detector';
-import { config, schema } from 'detector/config';
-import { getUserMedia } from 'utils';
-import appViewFactory from './view';
-import errorViewFactory from './view/error';
-import './styles';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
 
-document.body.appendChild(errorViewFactory().el);
-
-getUserMedia()
-  .then((videoStream) => {
-    const detector = detectorFactory(config);
-    const controls = Object.keys(schema).map((key) => schema[key]);
-
-    appViewFactory(detector, videoStream, controls);
-  })
-  .catch((err) => alert(err.message)); // eslint-disable-line no-alert
+ReactDOM.render(<App />, document.getElementById('root'));
