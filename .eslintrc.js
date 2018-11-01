32e2258cf8c87e86d23b4c7ad44c6c8fc839f3be
Autioch <autioch@gmail.com>
2018-11-01 22:22:27 +0100

    Comments linting relaxed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   },
   // plugins: ['react'],
   rules: {
+    'no-inline-comments': ['off'],
+    'line-comment-position': ['off'],
     // 'id-length': ['error', {min: 2, exceptions: ['h']}],
     // 'no-unused-vars': ['error', { 'varsIgnorePattern': 'h' }],
     // 'react/jsx-uses-vars': [2]
